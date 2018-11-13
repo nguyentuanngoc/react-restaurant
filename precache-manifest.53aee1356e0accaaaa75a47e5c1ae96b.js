6a93b63e3cdb7030a5ba39d2701d252e6a0aded0
Ngoc <ngocnguyen94nk@gmail.com>
2018-11-14 02:32:16 +0700

    Updates

---
+++ precache-manifest.53aee1356e0accaaaa75a47e5c1ae96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cab00eca9f3702697e8fb3ddf6709a1c",
+    "url": "/react-restaurant/static/media/homepage.cab00eca.jpeg"
+  },
+  {
+    "revision": "cda294134b5999f15f98",
+    "url": "/react-restaurant/static/js/runtime~main.cda29413.js"
+  },
+  {
+    "revision": "f961f7d9b52d93a368d9",
+    "url": "/react-restaurant/static/js/main.f961f7d9.chunk.js"
+  },
+  {
+    "revision": "843cb8751f1ded95b09d",
+    "url": "/react-restaurant/static/js/1.843cb875.chunk.js"
+  },
+  {
+    "revision": "f961f7d9b52d93a368d9",
+    "url": "/react-restaurant/static/css/main.be022647.chunk.css"
+  },
+  {
+    "revision": "013a5398fa4b9cd051eb396a686cf03d",
+    "url": "/react-restaurant/index.html"
+  }
+];
