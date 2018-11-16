ab3c6db5f05fc50903a1e3e54ca77be6ad0bb985
Ngoc <ngocnguyen94nk@gmail.com>
2018-11-17 05:30:10 +0700

    add some styles to About

--- src/About/about.js
+++ src/About/about.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import { about } from '../mock-data.json';
+import './about.scss';
 
 function Description() {
   const descriptionList = about.map((about, index) => {
     return (
-      <li key={index}>
-        <span>about.description</span>
-        <img src={process.env.PUBLIC_URL + about.image} alt="About us"></img>
+      <li className="description" key={index}>
+        <span className="descr-text">{about.description}</span>
+        <img className="descr-image" src={process.env.PUBLIC_URL + about.image} alt="About us"></img>
       </li>
     );
   });
-  return <ul>{descriptionList}</ul>
+  return <ul className="descr-list">{descriptionList}</ul>
 }
 export default class About extends Component {
   render() {
