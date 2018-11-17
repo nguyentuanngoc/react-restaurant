0a5b388acbadcfc3ace30a5a481a4cd193ca745c
Ngoc <ngocnguyen94nk@gmail.com>
2018-11-18 03:36:42 +0700

    Add book form

--- src/Book/book.js
+++ src/Book/book.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class Book extends Component {
   render() {
     return (
-      <div>
-        book
-      </div>
+      <form>
+        <label>Your name:<input type="text" name="name"></input></label>
+        <label>Your phone number:<input type="number" name="phone"></input></label>
+        <input type="submit" value="Submit"></input>
+      </form>
     )
   }
 }
